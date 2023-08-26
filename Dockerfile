32bed7aa0880359dce67557e9bdecc0c8922249f
sridhar1ga <sridharvanga2001@gmail.com>
2023-08-26 12:37:20 +0530

    added parallel

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ LABEL org.opencontainers.image.authors="sridharvanga2001@gmail.com"
 
 RUN apt-get update -y \
     && apt-get upgrade -y \
-    && apt-get -y install ffmpeg \
+    && apt-get -y install ffmpeg parallel \
     && export DEBIAN_FRONTEND=noninteractive
 
 RUN pip install --no-cache --upgrade pip setuptools
--- VrtParserPipeline/hello.sh
+++ VrtParserPipeline/hello.sh
@@ -6,10 +6,10 @@ source process_vrt.sh
 # $2 --> New output folder
 # $3 --> Mp4s folder
 
-#find $1 -type f -name *.vrt.gz | parallel -j 2 --bar process_audio $1 $2 $3 {}
+find $1 -type f -name *.vrt.gz | parallel -j 2 --halt soon,done=3 process_audio $1 $2 $3 {}
 
 
-for f in $(find $1 -type f -name *.vrt.gz); do
-	process_audio $1 $2 $3 $f
-done
+# for f in $(find $1 -type f -name *.vrt.gz); do
+# 	process_audio $1 $2 $3 $f
+# done
 
