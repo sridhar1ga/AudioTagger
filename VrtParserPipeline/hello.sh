6ee6ccdfa2b1287df70410ca5418fe1f9c6615c6
sridhar1ga <sridharvanga2001@gmail.com>
2023-08-27 13:11:46 +0530

    minor changes

--- VrtParserPipeline/hello.sh
+++ VrtParserPipeline/hello.sh
@@ -3,9 +3,9 @@ set -e
 # $1 --> folder of all the VRTs
 # $2 --> New output folder
 # $3 --> Mp4s folder
-source process_vrt.sh
+source /VrtParserPipeline/process_vrt.sh
 
-find $1 -type f -name *.vrt.gz | parallel -j 3 process_audio $1 $2 $3 {}
+find $1 -type f -name *.vrt.gz | parallel -j 4 process_audio $1 $2 $3 {}
 
 
 # for f in $(find $1 -type f -name *.vrt.gz); do
